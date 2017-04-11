78fa696bfe46d641723665bbdfe3b9ca6f66dd35
HADJER <HADJER@AQUAMARINE-PC>
2017-04-11 16:21:00 +0100

    Prediction Utilis added Evolution Utilis added

---
+++ src/Prediction/testPrediction.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Prediction;
+
+import evolutionIdentification.EvolutionUtils;
+
+/**
+ *
+ * @author HADJER
+ */
+public class testPrediction {
+    public static void main(String[] args) throws Exception {
+        String filePath = "./LibPrediction/";
+        String filename = "trainData";
+        String extension=".arff";
+        String BDpath="./LibEvolution/";
+        String BDfilename="testGED.db";
+        String tabname="GED";
+        int nbtimeframe=44;
+        
+        EvolutionUtils.writeEvolutionChain(BDpath, BDfilename, tabname,nbtimeframe/**nbre timeframes**/);
+        
+        //PredictionUtils.createClassifierJ48(filePath+filename+extension,10);
+        
+    }
+}
